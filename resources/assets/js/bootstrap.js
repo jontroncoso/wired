afe4e8a15f2f79f24cbcf57bd5c729d022bcaa88
Jon Troncoso <jon@tronco.so>
2018-08-19 23:23:54 -0600

    adding linting to circle

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -12,7 +12,9 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-} catch (e) {}
+} catch (e) {
+    // continue regardless of error
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,9 @@
 let mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('laravel-mix-eslint');
 
 mix.react('resources/assets/js/app.js', 'public/js')
+    .eslint({
+        fix: false,
+        cache: false,
+    })
    .sass('resources/assets/sass/app.scss', 'public/css');
