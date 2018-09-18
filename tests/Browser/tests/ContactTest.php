d1fa078c29b8f89b3e3952037968c0bb9141f76e
Jon <jon.troncoso@gmail.com>
2018-09-18 15:34:17 -0700

    renaming

--- tests/Browser/tests/ContactTest.php
+++ tests/Browser/tests/ContactTest.php
@@ -10,7 +10,7 @@ use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class LoginTest extends DuskTestCase
+class ContactTest extends DuskTestCase
 {
     /**
      * A Dusk test example.
